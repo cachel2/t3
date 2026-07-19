b907c9e22fcd951f5f2b657de3fb9db63fe95cac
Axel Ibarrondo <axeljoaquin2202@gmail.com>
2026-07-18 21:06:54 -0300

    lexer: implement lexer enum and its funcs

---
+++ crates/t3_lex/src/lexer.rs
@@ -0,0 +1,20 @@
+//! This is the t3 lexer file
+
+pub struct Lexer<'a> {
+    src: &'a [u8], // una referencia prestada al fuente
+    pos: usize,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(src: &'a [u8]) -> Self {
+        Lexer { src, pos: 0 }
+    }
+
+    fn peek(&self) -> Option<u8> {
+        self.src.get(self.pos).copied()
+    }
+
+    fn bump(&mut self) {
+        self.pos += 1
+    }
+}
